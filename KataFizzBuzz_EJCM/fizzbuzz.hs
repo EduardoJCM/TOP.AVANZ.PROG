e207df29c496a23c34ab3143ad8837605ee8b5f6
EduardoJCM <98174532+EduardoJCM@users.noreply.github.com>
2024-02-08 23:04:55 -0600

    Update fizzbuzz.hs

--- KataFizzBuzz_EJCM/fizzbuzz.hs
+++ KataFizzBuzz_EJCM/fizzbuzz.hs
@@ -1,6 +1,10 @@
 module FizzBuzz where
 
--- Función principal de FizzBuzz
+-- Funcion principal del FizzBuzz
+-- Si el numero que recibe es multiplo de 3 y 5 devuelve "FizzBuzz!".
+-- Si el numero que recibe es multiplo de 3 devuelve "Fizz!".
+-- Si el numero que recibe es multiplo de 5 devuelve "Buzz!".
+-- si no, pues devuelve el numero escrito
 fizzBuzz :: Int -> String
 fizzBuzz n
     | n `mod` 3 == 0 && n `mod` 5 == 0 = "FizzBuzz!"
@@ -9,14 +13,18 @@ fizzBuzz n
     | n >= 0 && n <= 100 = number n
     | otherwise = "Número fuera de rango"
 
--- Función para convertir números menores que 100 a su representación en palabras
+-- Funcion para convertir numeros menores que 100 en palabras
+-- Si el numero es menor que 20 llama a la función 'lessThan20'.
+-- Si el numero es multiplo de 10 llama a la función 'tens'.
+-- Y si no pues combina decena con unidad
 number :: Int -> String
 number n
     | n < 20 = lessThan20 n
     | n `mod` 10 == 0 = tens (n `div` 10)
     | otherwise = tens (n `div` 10) ++ " " ++ lessThan20 (n `mod` 10)
 
--- Función para convertir números menores que 20 a su representación en palabras
+-- Función para convertir numeros menores que 20 a palabras
+-- La función 'lessThan20' recibe un numero entero y devuelve el numero en palabras.
 lessThan20 :: Int -> String           
 lessThan20 n
     | n > 0 && n < 20 = 
@@ -25,14 +33,15 @@ lessThan20 n
                         "seventeen eighteen nineteen")
         in answers !! (n - 1) 
 
--- Función para convertir decenas a su representación en palabras
+-- Funcion para convertir decenas a palabras
+-- La función 'tens' recibe un numero entero y devuelve la decena en palabras.
 tens :: Int -> String
 tens n 
     | n > 1 && n <= 9 =
         let answers = words "twenty thirty forty fifty sixty seventy eighty ninety"
         in answers !! (n - 2)
-    | otherwise = "Número fuera de rango"
+    | otherwise = "Numero fuera de rango"
 
--- Función principal para probar fizzBuzz
+-- Funcion para probar fizzBuzz
 main :: IO ()
 main = mapM_ (putStrLn . fizzBuzz) [0..100]
